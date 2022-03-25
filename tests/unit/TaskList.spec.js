b4a1f6958858c541642fda3fed64ecd69ad123e0
jinwoo-choi-05 <user@AL01950857.local>
2022-03-25 09:57:45 +0900

    unit test with storybook

---
+++ tests/unit/TaskList.spec.js
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils';
+
+import TaskList from '../../src/components/TaskList.vue';
+import { WithPinnedTasks } from '../../src/components/TaskList.stories';
+
+test('render pinned tasks at the start of the list', () => {
+  const wrapper = mount(TaskList, {
+    props: WithPinnedTasks.args,
+  });
+  const firstPinnedTask = wrapper.find('.list-item:nth-child(1).TASK_PINNED');
+  expect(firstPinnedTask).not.toBe(null);
+});
